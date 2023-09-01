55deab9420357686a1cc876e90649894e35ad92a
Albert G <101777465+Albert-G-Dev@users.noreply.github.com>
2023-09-01 05:11:28 +0000

    X star

--- star.c
+++ star.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
-int main();
-{
-    int a;
-    scanf("%d",&a);
-    for(int i=1;i<=a;i++)
-    {
-        for (int j=1;j<=a;j++)
-        {
-    if (i==1||j==1||i==a||j==a)
-   {
-    printf("*");
 
-        }{
-            else
-            
-        }
+void main(){
+
+    int num=8;
+  
+    for(int i=1;i<num;i++){
+        for(int j=1;j<num;j++){
+            if(i==j){
+                 
+                 printf("*");
+            }
+            else if(i+j==num){
+                
+                printf("*");
+            }
+           else{
+                printf(" ");
+           }
+    }
+    printf("\n");
     }
 }
