31772c55e15aa5ec0f7f2e6d60cae1451513a04b
Albert G <101777465+Albert-G-Dev@users.noreply.github.com>
2023-09-01 04:41:44 +0000

    Palindrome Number

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+// pallindrome problem -> reversing a number equal to original number
+
+void main(){
+    int num;
+    printf("Enter a number :");
+    scanf("%d",&num);
+
+    int temp=num,rem=0,reverse=0;
+
+    while(num>0){
+        rem=num%10;
+        reverse=reverse*10+rem;
+        num/=10;
+    }
+    printf("Reverse : %d",reverse);
+    if(reverse==temp){
+        printf("\nThe number %d is pallindrome",temp);
+    }
+    else{
+         printf("\nThe number %d is not a pallindrome",temp);
+    }
+}
