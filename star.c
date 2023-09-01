4ea5487585fbb11df2d2b3158d80d346b2c1fe9d
Albert G <101777465+Albert-G-Dev@users.noreply.github.com>
2023-09-01 05:09:02 +0000

    traingle star

---
+++ star.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main();
+{
+    int a;
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++)
+    {
+        for (int j=1;j<=a;j++)
+        {
+    if (i==1||j==1||i==a||j==a)
+   {
+    printf("*");
+
+        }{
+            else
+            
+        }
+    }
+}
